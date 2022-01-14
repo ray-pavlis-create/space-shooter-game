8baaa6ee092d0ea516f27936a48146dcf60cb3b3
ray pavlis <61029871+ray-pavlis-create@users.noreply.github.com>
2022-01-14 12:35:16 -0700

    Add files via upload

--- space shooter game/main.py
+++ space shooter game/main.py
@@ -1,5 +1,6 @@
 import pygame
 from player import Player
+from player import Direction
 
 pygame.init()
 bounds = (300, 300)
@@ -8,3 +9,27 @@ pygame.display.set_caption("!Space Fighters!")
 
 block_size = 20
 player = Player(block_size, bounds)
+done = False
+
+while not done:
+    pygame.time.delay(100)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+        
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            player.steer(Direction.LEFT)
+        elif keys[pygame.K_RIGHT]:
+            player.steer(Direction.RIGHT)
+        elif keys[pygame.K_UP]:
+            player.steer(Direction.UP)
+        elif keys[pygame.K_DOWN]:
+            player.steer(Direction.DOWN)
+        
+    player.move()
+
+    window.fill((0, 0, 0))
+    player.draw(pygame, window)
+    pygame.display.update()
--- space shooter game/player.py
+++ space shooter game/player.py
@@ -1,26 +1,61 @@
 from enum import Enum
 
+
 class Direction(Enum):
     UP = 0
     DOWN = 1
     LEFT = 2
     RIGHT = 3
 
+
 class Player:
+    length = None
     direction = None
     body = None
     block_size = None
     color = (255, 255, 255)
     bounds = None
-
+    
     def __init__(self, block_size, bounds):
         self.block_size = block_size
         self.bounds = bounds
         self.respawn()
 
     def respawn(self):
-        #self.body = ?
-        self.direction = None
-
+        self.length = 1
+        self.body = [(20, 20)]
+        self.direction = Direction.DOWN
+    
     def draw(self, game, window):
-        game.draw.rect(window, self.color, self.block_size, self.block_size) #position???????
+        for segment in self.body:
+            game.draw.rect(
+                window, self.color,
+                (segment[0], segment[1], self.block_size, self.block_size))
+
+    def move(self):
+        curr_head = self.body[-1]
+        if self.direction == Direction.DOWN:
+            next_head = (curr_head[0], curr_head[1] + self.block_size)
+            self.body.append(next_head)
+        elif self.direction == Direction.UP:
+            next_head = (curr_head[0], curr_head[1] - self.block_size)
+            self.body.append(next_head)
+        elif self.direction == Direction.RIGHT:
+            next_head = (curr_head[0] + self.block_size, curr_head[1])
+            self.body.append(next_head)
+        elif self.direction == Direction.LEFT:
+            next_head = (curr_head[0] - self.block_size, curr_head[1])
+            self.body.append(next_head)
+
+        if self.length < len(self.body):
+            self.body.pop(0)
+
+    def steer(self, direction):
+        if self.direction == Direction.DOWN and direction != Direction.UP:
+            self.direction = direction
+        elif self.direction == Direction.UP and direction != Direction.DOWN:
+            self.direction = direction
+        elif self.direction == Direction.LEFT and direction != Direction.RIGHT:
+            self.direction = direction
+        elif self.direction == Direction.RIGHT and direction != Direction.LEFT:
+            self.direction = direction
